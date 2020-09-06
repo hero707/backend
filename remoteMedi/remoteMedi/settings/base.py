a43d199be5e177dc4d6ab40fe9c32859ecc32e4b
key kim <keyhyuk.kim@gmail.com>
2020-09-07 00:06:10 +0900

    Migrate zappa files

--- remoteMedi/remoteMedi/settings/base.py
+++ remoteMedi/remoteMedi/settings/base.py
@@ -83,12 +83,7 @@ WSGI_APPLICATION = 'remoteMedi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
@@ -122,7 +117,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
