e69bcb0fe78c3c2273a0bf3259eef71a81c5f53e
key kim <keyhyuk.kim@gmail.com>
2020-10-27 14:53:15 +0900

    Add CD pipeline

--- remoteMedi/remoteMedi/settings/debug.py
+++ remoteMedi/remoteMedi/settings/debug.py
@@ -1,8 +1,13 @@
 from .base import *
 
 DEBUG=True
-HOST = "http://127.0.0.1:10637"
+HOST = os.getenv("DEBUG_HOST")
 ALLOWED_HOSTS = ["*"]
+INSTALLED_APPS += ['django_s3_storage']
+S3_BUCKET = "zappa-sv7emz4ut"
+STATICFILES_STORAGE = "django_s3_storage.storage.StaticS3Storage"
+AWS_S3_BUCKET_NAME_STATIC = S3_BUCKET
+STATIC_URL = "https://%s.s3.amazonaws.com/" % S3_BUCKET
 
 AUTH_SERVER_LOGIN = ROOT_SERVER + "/login"
 
@@ -12,7 +17,20 @@ AUTH_SERVER_LOGOUT = ROOT_SERVER + "/logout"
 
 AUTH_SERVER_TOKEN = ROOT_SERVER + "/token"
 
-DATABASES = {
+
+if os.getenv("GITHUB_WORKFLOW"):
+   DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
+else: # .env
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME'), # dbname
